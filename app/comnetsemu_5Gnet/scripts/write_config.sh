bed56e13d981910e5995cbc4cfa6e04c4e684624
fabrizio-granelli <fabrizio.granelli@unitn.it>
2024-05-09 22:39:38 +0200

    Bug fixing

--- app/comnetsemu_5Gnet/scripts/write_config.sh
+++ app/comnetsemu_5Gnet/scripts/write_config.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
+# Check if eth1 exists
+if ip link show eth1 &>/dev/null; then
+    INTF="eth1"
+else
+    INTF="enp0s1"
+fi
+
+echo "INTF is set to: $INTF"
+
 # Get the IP address of the eth0 interface
-IP_ADDRESS=$(ip addr show enp0s1 | grep "inet\b" | awk '{print $2}' | cut -d/ -f1)
+IP_ADDRESS=$(ip addr show $INTF | grep "inet\b" | awk '{print $2}' | cut -d/ -f1)
 
 # Write the output to a text file
 echo -e "[MININET_SERVER]\nipaddr = $IP_ADDRESS\nport = 8000" > config.ini
