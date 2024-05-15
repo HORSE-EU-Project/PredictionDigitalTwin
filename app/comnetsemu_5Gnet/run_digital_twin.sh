0f8ecb20223c4149f24c8ca581be0641291ece31
fabrizio-granelli <fabrizio.granelli@unitn.it>
2024-05-15 14:42:13 +0200

    Some fixes

--- app/comnetsemu_5Gnet/run_digital_twin.sh
+++ app/comnetsemu_5Gnet/run_digital_twin.sh
@@ -1,4 +1,4 @@
-echo "*** Running RYU controller"
+echo "[\e[1;32m RYU \e[0m] *** Running RYU controller"
 ./scripts/run_ryu.sh &
-echo "*** Running Digital Twin Engine (Comnetsemu)"
+echo "[\e[1;32m NDT \e[0m] *** Running Digital Twin Engine (Comnetsemu)"
 sudo python3 DT_v0.7.py
