eacac1f9510074b20767dcbdc2c2ba5e3bc4b98f
Fabrizio Granelli <fabrizio.granelli@unitn.it>
2025-06-06 16:37:04 +0000

    Interfaces ready for testing

---
+++ app/P-and-P_Digital_Twin/HORSE_EM_interface/test_server.sh
@@ -0,0 +1,3 @@
+curl -X POST -F "file=@Threat_Model_NTP_attack v1.xml" http://127.0.0.1:5000/upload
+echo
+
---
+++ app/P-and-P_Digital_Twin/run_DEMO_0.sh
@@ -0,0 +1,6 @@
+cd ./HORSE_EM_interface/
+./run_EM_interface.sh &
+cd ..
+sleep 10
+cd ./HORSE_DTE_interface/
+./test_DTE.sh
