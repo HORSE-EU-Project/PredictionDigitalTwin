14e8be1f48b5f47c77afe4efb4ce6b8b1318a3d3
Fabrizio Granelli <fabrizio.granelli@unitn.it>
2025-10-05 16:04:44 +0000

    Introducing NDT dashboard

--- app/P-and-P_Digital_Twin/run_DEMO_0_UPC.sh
+++ app/P-and-P_Digital_Twin/run_DEMO_0_UPC.sh
@@ -3,11 +3,15 @@ figlet "HORSE P&P NDT"
 figlet -f ./scripts/smkeyboard.flf "DEMO#0"
 echo
 echo "[HORSE SAN] Collecting the topology file from SM and storing it in the /data directory"
-curl http://10.19.2.15:8000/file -o data/topology.txt
+#curl http://10.19.2.15:8000/file -o data/topology.txt
+python3 HORSE_dashboard/update_senderv2.py box2 green "NDT Ready"
+sleep 5
 echo 
 echo "[HORSE SAN] Activating EM Interface"
 cd ./HORSE_EM_interface/
 ./run_EM_interface_UPC.sh &
+python3 HORSE_dashboard/update_senderv2.py box1 green "EM Message Received"
 sleep 10
 ./monitor_file_UPC.sh ./last.xml
+python3 HORSE_dashboard/update_senderv2.py box3 green "Message to DTE Sent"
 
