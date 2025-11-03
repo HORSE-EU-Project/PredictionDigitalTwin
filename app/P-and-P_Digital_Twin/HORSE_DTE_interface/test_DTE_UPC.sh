35d3e12260cbd5c7dfd243b1ef86344cf7f93d88
Fabrizio Granelli <fabrizio.granelli@unitn.it>
2025-11-03 17:31:11 +0000

    DEMO#0 ready for recording

--- app/P-and-P_Digital_Twin/HORSE_DTE_interface/test_DTE_UPC.sh
+++ app/P-and-P_Digital_Twin/HORSE_DTE_interface/test_DTE_UPC.sh
@@ -1,3 +1,7 @@
+echo "[ {"
+echo " \"prevention\": \"ddos_downlink\", "
+echo " \"confidence\": 0.5"
+echo "} ]"
 curl -X 'POST' \
   'http://10.19.2.1:9898/receive-data/' \
   -H 'accept: application/json' \
