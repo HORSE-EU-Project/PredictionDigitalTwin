2fa11fd4a4e655c2659a810f079111ca556e499b
Vagrant user <vagrant@netdigitaltwin.novalocal>
2026-01-08 15:50:39 +0000

    DEMOs #4 and #10 recording setup

--- app/P-and-P_Digital_Twin/HORSE_EM_interface/test_server.sh
+++ app/P-and-P_Digital_Twin/HORSE_EM_interface/test_server.sh
@@ -1,3 +1,3 @@
-curl -X POST -F "file=@Threat_Model_DNS_Amplification v1.xml" http://127.0.0.1:5000/upload
+curl -X POST -F "file=@Threat_Model_DDoS_Downlink.xml" http://127.0.0.1:5000/upload
 echo
 
